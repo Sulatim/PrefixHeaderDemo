93e9e617eac320b25449a99bb40b7da76300d19b
CHX <softapprentice@gmail.com>
2017-07-28 11:18:54 +0800

    Xcode 8 PrefixHeader Setting

---
+++ demoPch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  demoPch
+//
+//  Created by CHX on 2017/7/24.
+//  Copyright © 2017年 chx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ demoPch/Config.h
@@ -0,0 +1,15 @@
+//
+//  Config.h
+//  demoPch
+//
+//  Created by CHX on 2017/7/24.
+//  Copyright © 2017年 chx. All rights reserved.
+//
+
+#ifndef Config_h
+#define Config_h
+
+
+#define isDebug YES
+
+#endif /* Config_h */
